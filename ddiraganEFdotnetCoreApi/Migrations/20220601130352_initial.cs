292c56bfbebaecdf1eae469fa5656c8ac5729446
dogukanbey <dogukandiragan@hotmail.com>
2022-06-02 07:50:22 +0300

    Commit1

---
+++ ddiraganEFdotnetCoreApi/Migrations/20220601130352_initial.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ddiraganEFdotnetCoreApi.Migrations
+{
+    public partial class initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    ProductID = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    ProductName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CategoryID = table.Column<int>(type: "int", nullable: false),
+                    Price = table.Column<double>(type: "float", nullable: false),
+                    TotalStock = table.Column<int>(type: "int", nullable: false),
+                    LastOrderDate = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.ProductID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Products");
+        }
+    }
+}
---
+++ ddiraganEFdotnetCoreApi/Repository/LinkContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ddiraganEFdotnetCoreApi.Models;
+
+namespace ddiraganEFdotnetCoreApi.Repository
+{
+    public class LinkContext :DbContext
+    {
+
+        public DbSet<Product>? Products { get; set; }
+
+        public LinkContext(DbContextOptions<LinkContext> options)
+         : base(options)
+        {
+        }
+
+
+
+    }
+}
