b7adfcea24f2865825f04b5ca251705716bac346
dogukanbey <dogukandiragan@hotmail.com>
2022-06-03 18:07:37 +0300

    1stCommit

---
+++ EFCoreApiExam/Controllers/ProductController.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using diragansEFCoreApiExam1.Models;
+using diragansEFCoreApiExam1.Repository;
+
+namespace diragansEFCoreApiExam1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+
+        private readonly IProductRepository _productRepository;
+        public ProductController(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
+
+        //Get all products
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            try
+            {
+                return Ok(_productRepository.GetAllProduct());
+            }
+            catch { return NotFound(); }
+        }
+
+
+        //Get 1 product by id
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            try
+            {
+                return Ok(_productRepository.GetProductByID(id));
+            }
+            catch { return NotFound(); }
+        }
+
+
+
+        //Insert a new product record
+        [HttpPost]
+        public IActionResult Add([FromBody] Product product)
+        {
+            try
+            {
+                _productRepository.AddProduct(product);
+                return Ok();
+            }
+            catch { return BadRequest(); }
+        }
+
+
+
+
+        //Edit a product 
+        [HttpPut]
+        public IActionResult Update(Product product)
+        {
+            try
+            {
+                _productRepository.UpdateProduct(product);
+                return Ok(product);
+            }
+            catch { return BadRequest(); }
+        }
+
+
+
+        //Delete a product by ID
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            try
+            {
+                _productRepository.DeleteProduct(id);
+                return Ok($"Deleted ID:{id}");
+            }
+            catch { return BadRequest(); }
+        }
+
+
+
+    }
+}
---
+++ EFCoreApiExam/Migrations/20220602032902_initial.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace diragansEFCoreApiExam1.Migrations
+{
+    public partial class initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    ProductID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ProductName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CategoryID = table.Column<int>(type: "int", nullable: false),
+                    Price = table.Column<double>(type: "float", nullable: false),
+                    TotalStock = table.Column<int>(type: "int", nullable: false),
+                    LastOrderDate = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.ProductID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Products");
+        }
+    }
+}
