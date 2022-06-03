52f1b649e6f4c1fe5aa8d5d1d31199c2cc8ad048
dogukanbey <dogukandiragan@hotmail.com>
2022-06-03 15:57:02 +0300

    3rdCommit

---
+++ diragansEFCoreApiExam1/Repositories/LinkContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using diragansEFCoreApiExam1.Models;
+
+
+namespace diragansEFCoreApiExam1.Repository
+{
+    public class LinkContext : DbContext
+    {
+
+        public LinkContext(DbContextOptions<LinkContext> options) 
+            : base(options)
+        {
+        }
+
+        //Setting Entity Set
+        public DbSet<Product> Products { get; set; }
+ 
+       
+ 
+ 
+    }
+}
